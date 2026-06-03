368cf9de109635b1a10c5764888e25f822b11561
Paloys <40764414+Paloys@users.noreply.github.com>
2026-06-03 11:35:42 +0200

    try for firefox 3

--- src/frontend/src/features/rooms/components/Conference.tsx
+++ src/frontend/src/features/rooms/components/Conference.tsx
@@ -90,7 +90,7 @@ export const Conference = ({
           const wssUrl =
             apiConfig.galene.url
               .replace('https://', 'wss://')
-              .replace(/\/$/, '') + '/'
+              .replace(/\/$/, '') + '/ws'
 
           /**
            * FIREFOX + PROXY WORKAROUND:
